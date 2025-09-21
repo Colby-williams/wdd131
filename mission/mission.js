3cceb6ac07b20eec890655806e05d4f645a7f2ea
Colby-williams <wil21128@byui.edu>
2025-09-20 18:47:17 -0600

    Add theme selector and dark mode functionality with logo updates

---
+++ mission/mission.js
@@ -0,0 +1,15 @@
+const themeSelector = document.querySelector(".theme-selector");
+const log = document.querySelector(".logo");
+
+function changeTheme() {
+    const selectedTheme = themeSelector.value;
+    if (selectedTheme === "dark") {
+        document.body.classList.add("dark");
+        log.src = "white-logo.jpg";
+    } else {
+        document.body.classList.remove("dark")
+        log.src = "blue-logo.webp";
+    }
+}
+
+themeSelector.addEventListener("change", changeTheme);
