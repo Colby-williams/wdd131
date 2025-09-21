7756a47e4055afd71bc975f84b61e1c72395460d
Colby-williams <wil21128@byui.edu>
2025-09-20 18:58:20 -0600

    Final touch ups to Mission Statement project

--- mission/mission.js
+++ mission/mission.js
@@ -5,7 +5,7 @@ function changeTheme() {
     const selectedTheme = themeSelector.value;
     if (selectedTheme === "dark") {
         document.body.classList.add("dark");
-        log.src = "white-logo.jpg";
+        log.src = "byui-logo_white.png";
     } else {
         document.body.classList.remove("dark")
         log.src = "blue-logo.webp";
